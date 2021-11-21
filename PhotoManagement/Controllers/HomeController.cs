458291e81aab013a08bf58c94686b5b24334c332
TRMogane <27252396@student.g.nwu.ac.za>
2021-11-21 13:14:09 +0200

    Picture Uploader

--- PhotoManagement/Controllers/HomeController.cs
+++ PhotoManagement/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using PhotoManagement.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,15 +21,15 @@ namespace PhotoManagement.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+      //  public HomeController(ILogger<HomeController> logger)
+       // {
+       //     _logger = logger;
+       // }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+       // public IActionResult Index()
+       // {
+          //  return View();
+       // }
 
         public IActionResult Privacy()
         {
@@ -34,6 +40,142 @@ namespace PhotoManagement.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        static CloudBlobClient blobClient;
+        const string blobContainerName = "imagecontainer";
+        private readonly IConfiguration configuration;
+        static CloudBlobContainer blobContainer;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var storageConnectionString = configuration.GetValue<string>("StorageConnectionString");
+
+                var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+                blobClient = storageAccount.CreateCloudBlobClient();
+                blobContainer = blobClient.GetContainerReference(blobContainerName);
+                await blobContainer.CreateIfNotExistsAsync();
+                await blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+                List<Uri> allBlobs = new List<Uri>();
+                BlobContinuationToken blobContinuationToken = null;
+                do {
+                    var responce = await blobContainer.ListBlobsSegmentedAsync(blobContinuationToken);
+                    foreach (IListBlobItem blob in responce.Results)
+                    {
+                        if (blob.GetType() == typeof(CloudBlockBlob))
+                            allBlobs.Add(blob.Uri);
+                    }
+
+                    blobContinuationToken = responce.ContinuationToken;
+                } while (blobContinuationToken != null);
+
+                return View(allBlobs);
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UploadAsync()
+        {
+            try
+            {
+                var request = await HttpContext.Request.ReadFormAsync();
+                if(request.Files == null)
+                {
+                    return BadRequest("Could not upload files");
+                }
+                var files = request.Files;
+                if(files.Count == 0)
+                {
+                    return BadRequest("No files selected");
+                }
+
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        var blob = blobContainer.GetBlockBlobReference(GetRandomBlobName(files[i].FileName));
+                        using(var stream = files[i].OpenReadStream())
+                        {
+                            await blob.UploadFromStreamAsync(stream);
+                        }
+                    }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteImage(string name)
+        {
+            try
+            {
+                Uri uri = new Uri(name);
+                string filename = Path.GetFileName(uri.LocalPath);
+
+                var blob = blobContainer.GetBlockBlobReference(filename);
+                await blob.DeleteIfExistsAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteAll()
+        {
+            try
+            {
+                BlobContinuationToken blobContinuationToken = null;
+                do
+                {
+                    var responce = await blobContainer.ListBlobsSegmentedAsync(blobContinuationToken);
+                    foreach (IListBlobItem blob in responce.Results)
+                    {
+                        if (blob.GetType() == typeof(CloudBlockBlob))
+                            await ((CloudBlockBlob)blob).DeleteIfExistsAsync();
+                    }
+
+                    blobContinuationToken = responce.ContinuationToken;
+                } while (blobContinuationToken != null);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        private string GetRandomBlobName(string filename)
+        {
+            string ext = Path.GetExtension(filename);
+            return string.Format("{0:10}_{1}{2}", DateTime.Now.Ticks, Guid.NewGuid(), ext);
         }
     }
 }
--- PhotoManagement/Startup.cs
+++ PhotoManagement/Startup.cs
@@ -8,6 +8,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Blobs;
+using Azure.Core.Extensions;
 
 namespace PhotoManagement
 {
@@ -25,6 +29,11 @@ namespace PhotoManagement
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddAzureClients(builder =>
+            {
+                builder.AddBlobServiceClient(Configuration["ConnectionStrings:StorageConnectionString:blob"], preferMsi: true);
+                builder.AddQueueServiceClient(Configuration["ConnectionStrings:StorageConnectionString:queue"], preferMsi: true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +64,31 @@ namespace PhotoManagement
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+    }
+    internal static class StartupExtensions
+    {
+        public static IAzureClientBuilder<BlobServiceClient, BlobClientOptions> AddBlobServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddBlobServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddBlobServiceClient(serviceUriOrConnectionString);
+            }
+        }
+        public static IAzureClientBuilder<QueueServiceClient, QueueClientOptions> AddQueueServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddQueueServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddQueueServiceClient(serviceUriOrConnectionString);
+            }
         }
     }
 }

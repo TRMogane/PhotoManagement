1d1dbca344dd3f741542f53ce5a151fd6ca41d26
TRMogane <27252396@student.g.nwu.ac.za>
2021-11-21 09:37:38 +0200

    Complete Login

--- PhotoManagement/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PhotoManagement/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -57,6 +57,11 @@ namespace PhotoManagement.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            if (User.Identity.IsAuthenticated) { 
+                Response.Redirect("/");
+            }
+                
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);

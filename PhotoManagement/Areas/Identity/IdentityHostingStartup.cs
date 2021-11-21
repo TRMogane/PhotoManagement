5c479286e0af8c559eadf8868d8b842c763e5dcb
TRMogane <27252396@student.g.nwu.ac.za>
2021-11-21 07:44:06 +0200

    Add project files.

---
+++ PhotoManagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhotoManagement.Areas.Identity.Data;
+using PhotoManagement.Data;
+
+[assembly: HostingStartup(typeof(PhotoManagement.Areas.Identity.IdentityHostingStartup))]
+namespace PhotoManagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PhotoManagementContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PhotoManagementContextConnection")));
+
+                services.AddDefaultIdentity<PhotoManagementUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<PhotoManagementContext>();
+            });
+        }
+    }
+}
